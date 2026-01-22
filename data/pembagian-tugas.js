20c1202d31fa761245185d623e5b929cc60b85e5
Adyantto <adiyantond@gmail.com>
2026-01-22 16:55:17 +0800

    Update pembagian-tugas.js

--- data/pembagian-tugas.js
+++ data/pembagian-tugas.js
@@ -39,6 +39,7 @@ const pembagianTugas = [
   { username:"Anastasia Rendo", jenis:"mengajar", uraian:"Agama Katolik", kelas:"XI E–H", jp:12 },
   { username:"Anastasia Rendo", jenis:"mengajar", uraian:"Agama Katolik", kelas:"X A–H", jp:24 },
   { username:"Anastasia Rendo", jenis:"tambahan", uraian:"Guru Wali", kelas:"", jp:2 },,
+   { username:"Anastasia Rendo", jenis:"tambahan", uraian:"Guru Piket", kelas:"", jp:1 },
 
   /* ========== ELISIUS JUNAIDI ========== */
   { username:"199206142020121010", jenis:"mengajar", uraian:"PJOK", kelas:"XII A–D", jp:12 },
@@ -50,6 +51,7 @@ const pembagianTugas = [
   { username:"197111212007012015", jenis:"mengajar", uraian:"SBK", kelas:"XI A–D", jp:8 },
   { username:"197111212007012015", jenis:"mengajar", uraian:"Muatan Lokal", kelas:"X A–H", jp:16 },
   { username:"197111212007012015", jenis:"tambahan", uraian:"Guru Wali", kelas:"", jp:2 },
+   { username:"197111212007012015", jenis:"tambahan", uraian:"Guru Piket", kelas:"", jp:1 },
   
   /* ========== ENITA ========== */
   { username:"198712042024212018", jenis:"mengajar", uraian:"Fisika", kelas:"XII A–B, G", jp:20 },
@@ -67,6 +69,7 @@ const pembagianTugas = [
   { username:"198911302024212019", jenis:"mengajar", uraian:"SBK", kelas:"XII A–D", jp:8 },
   { username:"198911302024212019", jenis:"mengajar", uraian:"Agama Kristen", kelas:"X A–D", jp:12 },
   { username:"198911302024212019", jenis:"tambahan", uraian:"Guru Wali", kelas:"", jp:2 },,
+   { username:"198911302024212019", jenis:"tambahan", uraian:"Guru Piket", kelas:"", jp:1 },
 
   /* ========== FETRUS FREDY ========== */
   { username:"198805252024211015", jenis:"mengajar", uraian:"Fisika", kelas:"XI A–B, E–F", jp:20 },
@@ -113,6 +116,7 @@ const pembagianTugas = [
   { username:"199712312025211047", jenis:"mengajar", uraian:"SBK", kelas:"XI G–H", jp:4 },
   { username:"199712312025211047", jenis:"mengajar", uraian:"Agama Islam", kelas:"X A–H", jp:24 },
   { username:"199712312025211047", jenis:"tambahan", uraian:"Guru Wali", kelas:"", jp:2 },
+   { username:"199712312025211047", jenis:"tambahan", uraian:"Guru Piket", kelas:"", jp:1 },
 
   /* ========== JUFRIN ========== */
   { username:"198501252022211016", jenis:"mengajar", uraian:"Sosiologi", kelas:"XII C–D", jp:10 },
@@ -124,6 +128,7 @@ const pembagianTugas = [
   { username:"199502092025212063", jenis:"mengajar", uraian:"Matematika", kelas:"XI E, F, H", jp:12 },
   { username:"199502092025212063", jenis:"mengajar", uraian:"Matematika (Tk. Lanjut)", kelas:"XI A–B, G", jp:15 },
   { username:"199502092025212063", jenis:"tambahan", uraian:"Guru Wali", kelas:"", jp:2 },,
+   { username:"199502092025212063", jenis:"tambahan", uraian:"Guru Piket", kelas:"", jp:1 },
 
   /* ========== LISTIANA ========== */
   { username:"199207252022212013", jenis:"mengajar", uraian:"Kimia", kelas:"XI B, C, D", jp:15 },
@@ -135,6 +140,7 @@ const pembagianTugas = [
   { username:"198207282025211048", jenis:"mengajar", uraian:"Agama Islam", kelas:"XI A–D", jp:12 },
   { username:"198207282025211048", jenis:"mengajar", uraian:"SBK", kelas:"XI E–F", jp:4 },
   { username:"198207282025211048", jenis:"tambahan", uraian:"Guru Wali", kelas:"", jp:2 },,
+   { username:"198207282025211048", jenis:"tambahan", uraian:"Guru Piket", kelas:"", jp:1 },
 
   /* ========== MAHDALENA ========== */
   { username:"199209292023212018", jenis:"mengajar", uraian:"Bahasa Indonesia", kelas:"XI E–F", jp:8 },
@@ -146,32 +152,38 @@ const pembagianTugas = [
   { username:"197903192024211002", jenis:"mengajar", uraian:"SBK", kelas:"XII E–H", jp:8 },
   { username:"197903192024211002", jenis:"mengajar", uraian:"Agama Kristen", kelas:"X E–H", jp:12 },
   { username:"197903192024211002", jenis:"tambahan", uraian:"Guru Wali", kelas:"", jp:2 },,
+   { username:"197903192024211002", jenis:"tambahan", uraian:"Guru Piket", kelas:"", jp:1 },
 
   /* ========== MARIA DWI O.L. ========== */
   { username:"199910222025212029", jenis:"mengajar", uraian:"Sejarah", kelas:"XII A–H", jp:16 },
   { username:"199910222025212029", jenis:"mengajar", uraian:"Sejarah", kelas:"XI A–H", jp:16 },
   { username:"199910222025212029", jenis:"tambahan", uraian:"Guru Wali", kelas:"", jp:2 },,
+   { username:"199910222025212029", jenis:"tambahan", uraian:"Guru Piket", kelas:"", jp:1 },
 
   /* ========== MARLIN MARIO EVA SUMUAL ========== */
   { username:"197303142024212003", jenis:"mengajar", uraian:"Agama Kristen", kelas:"XI A–H", jp:24 },
   { username:"197303142024212003", jenis:"tambahan", uraian:"Guru Wali", kelas:"", jp:2 },,
+   { username:"197303142024212003", jenis:"tambahan", uraian:"Guru Piket", kelas:"", jp:1 },
 
   /* ========== MEYDI SUSANTI ========== */
   { username:"198105202011012007", jenis:"mengajar", uraian:"Ekonomi", kelas:"XII E–F, H", jp:15 },
   { username:"198105202011012007", jenis:"mengajar", uraian:"Ekonomi", kelas:"XI E–F", jp:10 },
   { username:"198105202011012007", jenis:"tambahan", uraian:"Koor PKB/PKG", kelas:"", jp:2 },,
   { username:"198105202011012007", jenis:"tambahan", uraian:"Guru Wali", kelas:"", jp:2 },,
+   { username:"198105202011012007", jenis:"tambahan", uraian:"Guru Piket", kelas:"", jp:1 },
 
   /* ========== MEYLINDa ========== */
   { username:"199505052024212077", jenis:"mengajar", uraian:"Informatika", kelas:"XII F–H", jp:15 },
   { username:"199505052024212077", jenis:"mengajar", uraian:"Informatika", kelas:"XI G–H", jp:10 },
   { username:"199505052024212077", jenis:"mengajar", uraian:"Koding dan AI", kelas:"X A–H", jp:16 },
   { username:"199505052024212077", jenis:"tambahan", uraian:"Guru Wali", kelas:"", jp:2 },,
+   { username:"199505052024212077", jenis:"tambahan", uraian:"Guru Piket", kelas:"", jp:1 },
 
   /* ========== MIRA HENNY ASMIATI ========== */
   { username:"198606212009022001", jenis:"mengajar", uraian:"Geografi", kelas:"XII E–F, G", jp:15 },
   { username:"198606212009022001", jenis:"mengajar", uraian:"Geografi", kelas:"XI E–F", jp:10 },
   { username:"198606212009022001", jenis:"tambahan", uraian:"Guru Wali", kelas:"", jp:2 },,
+   { username:"198606212009022001", jenis:"tambahan", uraian:"Guru Piket", kelas:"", jp:1 },
 
   /* ========== MUHAMMAD AFWAN MAULANA ========== */
   { username:"200001242024211004", jenis:"mengajar", uraian:"Geografi", kelas:"XI A–B", jp:10 },
@@ -183,6 +195,7 @@ const pembagianTugas = [
   { username:"198902022024211015", jenis:"mengajar", uraian:"Bahasa Indonesia", kelas:"XI G–H", jp:8 },
   { username:"198902022024211015", jenis:"mengajar", uraian:"Bahasa Indonesia", kelas:"X E–H", jp:16 },
   { username:"198902022024211015", jenis:"tambahan", uraian:"Guru Wali", kelas:"", jp:2 },,
+   { username:"198902022024211015", jenis:"tambahan", uraian:"Guru Piket", kelas:"", jp:1 },
 
   /* ========== MULYADI ========== */
   { username:"198310102022211022", jenis:"mengajar", uraian:"PKN", kelas:"XII C, D, E, F, G", jp:10 },
@@ -214,6 +227,7 @@ const pembagianTugas = [
   /* ========== NOPITA PUTRI NASARI ========== */
   { username:"199411062025212073", jenis:"mengajar", uraian:"Matematika", kelas:"XI G", jp:4 },
   { username:"199411062025212073", jenis:"mengajar", uraian:"Matematika", kelas:"X D–H", jp:20 },
+   { username:"199411062025212073", jenis:"tambahan", uraian:"Guru Piket", kelas:"", jp:1 },
 
   /* ========== NURINTAN TOGATOROP ========== */
   { username:"198711222025212023", jenis:"mengajar", uraian:"Ekonomi", kelas:"X A–H", jp:24 },
@@ -225,6 +239,7 @@ const pembagianTugas = [
   { username:"199108242025212058", jenis:"mengajar", uraian:"Sosiologi", kelas:"X F–H", jp:9 },
   { username:"199108242025212058", jenis:"mengajar", uraian:"Sejarah", kelas:"X F–H", jp:6 },
   { username:"199108242025212058", jenis:"tambahan", uraian:"Guru Wali", kelas:"", jp:2 },,
+   { username:"199108242025212058", jenis:"tambahan", uraian:"Guru Piket", kelas:"", jp:1 },
 
   /* ========== PERAWATI ========== */
   { username:"198602272025212025", jenis:"mengajar", uraian:"Bahasa Inggris", kelas:"X A–F", jp:18 },
@@ -254,6 +269,7 @@ const pembagianTugas = [
   { username:"Rifky Ibnu Abdul Aziz", jenis:"mengajar", uraian:"Bimbingan Konseling", kelas:"XII A–D", jp:8 },
   { username:"Rifky Ibnu Abdul Aziz", jenis:"mengajar", uraian:"Bimbingan Konseling", kelas:"XI A–H", jp:16 },
   { username:"Rifky Ibnu Abdul Aziz", jenis:"tambahan", uraian:"Guru Wali", kelas:"", jp:2 },,
+   { username:"Rifky Ibnu Abdul Aziz", jenis:"tambahan", uraian:"Guru Piket", kelas:"", jp:1 },
 
   /* ========== RILATI SRI WAHYUNI ========== */
   { username:"199604272024212035", jenis:"mengajar", uraian:"Kimia", kelas:"XI G–H", jp:10 },
@@ -266,6 +282,7 @@ const pembagianTugas = [
   { username:"198903112025212023", jenis:"mengajar", uraian:"Biologi", kelas:"X D–H", jp:15 },
   { username:"198903112025212023", jenis:"tambahan", uraian:"Pembina UKS", kelas:"", jp:2 },,
   { username:"198903112025212023", jenis:"tambahan", uraian:"Guru Wali", kelas:"", jp:2 },
+   { username:"198903112025212023", jenis:"tambahan", uraian:"Guru Piket", kelas:"", jp:1 },
 
   /* ========== SATIA LIRI YANTI ========== */
   { username:"199408092024212035", jenis:"mengajar", uraian:"PJOK", kelas:"XII E–H", jp:12 },
@@ -287,6 +304,7 @@ const pembagianTugas = [
   /* ========== SOPIANA ========== */
   { username:"197908162024212003", jenis:"mengajar", uraian:"Agama Kristen", kelas:"XII A–H", jp:24 },
   { username:"197908162024212003", jenis:"tambahan", uraian:"Guru Wali", kelas:"", jp:2 },,
+   { username:"197908162024212003", jenis:"tambahan", uraian:"Guru Piket", kelas:"", jp:1 },
 
   /* ========== STIVAN MARGUS PICASOUW ========== */
   { username:"198403052022211022", jenis:"mengajar", uraian:"Bahasa Inggris", kelas:"XII A–G", jp:21 },
@@ -298,17 +316,20 @@ const pembagianTugas = [
   { username:"198110172010012022", jenis:"mengajar", uraian:"Kimia", kelas:"XII A–D", jp:20 },
   { username:"198110172010012022", jenis:"tambahan", uraian:"Waka Sapras", kelas:"", jp:12 },
   { username:"198110172010012022", jenis:"tambahan", uraian:"Guru Wali", kelas:"", jp:2 },,
+   { username:"198110172010012022", jenis:"tambahan", uraian:"Guru Piket", kelas:"", jp:1 },
 
   /* ========== SUSILAWATI ========== */
   { username:"198006132005022004", jenis:"mengajar", uraian:"Biologi", kelas:"XII C–D, H", jp:15 },
   { username:"198006132005022004", jenis:"tambahan", uraian:"Waka Humas", kelas:"", jp:12 },
   { username:"198006132005022004", jenis:"tambahan", uraian:"Penilai Kinerja Guru (PKG)", kelas:"", jp:2 },,
   { username:"198006132005022004", jenis:"tambahan", uraian:"Guru Wali", kelas:"", jp:2 },,
+   { username:"198006132005022004", jenis:"tambahan", uraian:"Guru Piket", kelas:"", jp:1 },
 
   /* ========== VIDRICH DENDI ========== */
   { username:"Vidrich Dendi", jenis:"mengajar", uraian:"Bimbingan Konseling", kelas:"XII E–H", jp:8 },
   { username:"Vidrich Dendi", jenis:"mengajar", uraian:"Bimbingan Konseling", kelas:"X A–H", jp:16 },
   { username:"Vidrich Dendi", jenis:"tambahan", uraian:"Guru Wali", kelas:"", jp:2 },,
+   { username:"Vidrich Dendi", jenis:"tambahan", uraian:"Guru Piket", kelas:"", jp:1 },
 
   /* ========== YOHANA FRANSISKA SELLA ========== */
   { username:"199210132024212038", jenis:"mengajar", uraian:"Kimia", kelas:"XII E–F, H", jp:15 },
@@ -323,6 +344,7 @@ const pembagianTugas = [
   { username:"198212282024211011", jenis:"tambahan", uraian:"Koor. Projek Kelas XI D", kelas:"", jp:2 },,
   { username:"198212282024211011", jenis:"tambahan", uraian:"Koor. Projek Kelas XI E", kelas:"", jp:2 },,
   { username:"198212282024211011", jenis:"tambahan", uraian:"Guru Wali", kelas:"", jp:2 },,
+   { username:"198212282024211011", jenis:"tambahan", uraian:"Guru Piket", kelas:"", jp:1 },
 
   /* ========== YUDHA KALVARI ========== */
   { username:"198912262025211011", jenis:"mengajar", uraian:"Sastra Inggris (Tk. Lanjut)", kelas:"XII C–D, G", jp:15 },
@@ -337,6 +359,7 @@ const pembagianTugas = [
   { username:"Yuvenalis Raga Gening", jenis:"mengajar", uraian:"Agama Katolik", kelas:"XII A–H", jp:24 },
   { username:"Yuvenalis Raga Gening", jenis:"mengajar", uraian:"Agama Katolik", kelas:"XI A–D", jp:12 },
   { username:"Yuvenalis Raga Gening", jenis:"tambahan", uraian:"Guru Wali", kelas:"", jp:2 },
+   { username:"Yuvenalis Raga Gening", jenis:"tambahan", uraian:"Guru Piket", kelas:"", jp:1 },
 
 ];
 
